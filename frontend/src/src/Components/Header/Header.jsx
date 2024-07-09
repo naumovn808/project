d1d0b44a6f36975263ac05678a7473e7a010a088
onlive1337 <tabdulov2006@gmail.com>
2024-07-09 05:03:25 +0500

    Starting to work on front side (kinda footer and header) need to finis this for sure

---
+++ frontend/src/src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+const header = () => (
+    <div className='header'>
+        <img src="/Partyshaker.png" alt="Partyshaker"/>
+    </div>
+);
+
+export default header;
