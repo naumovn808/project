3998eda753906aa08d0037ab1d6f0df1e5042f73
onlive1337 <tabdulov2006@gmail.com>
2024-07-12 19:28:08 +0500

    Entire version of the footer (не ищите баги пж)

--- frontend/src/components/Auth_Footer/Auth_Footer.jsx
+++ frontend/src/components/Auth_Footer/Auth_Footer.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import styles from './Auth_Footer.module.css';
+import React from "react";
+import styles from "./Auth_Footer.module.css";
 
 const Footer = () => (
-    <div className={styles.footer}>
-        <div className={styles.footerLeft}>
-            <div className={styles.round}>
-                <img src="/footer_logo.png" alt="PartyShaker Logo" className={styles.footerLogo} />
-            </div>
-            <div className={styles.container}>
-            <div className={styles.footerInfo}>
-                <p className={styles.footer_p}>Partyshaker</p>
-                <p><a className={styles.footer_a} href="mailto:info@partyshaker.ru">info@partyshaker.ru</a></p>            
-                </div>            
-                </div>
-                </div>
-                <div className={styles.line}></div>
-        <div className={styles.footerRight}>
-            <a href="#">Политика конфиденциальности</a>
-            </div>
+  <div className={styles.footer}>
+    <div className={styles.footerLeft}>
+      <div className={styles.round}>
+        <img src="/footer_logo.png" alt="PartyShaker Logo" className={styles.footerLogo} />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.footerInfo}>
+          <p className={styles.footer_p}>Partyshaker</p>
+          <p>
+            <a className={styles.footer_a} href="mailto:info@partyshaker.ru">
+              info@partyshaker.ru
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
+    <div className={styles.footerRight}>
+      <a href="#">Политика конфиденциальности</a>
+    </div>
+  </div>
 );
 
 export default Footer;
