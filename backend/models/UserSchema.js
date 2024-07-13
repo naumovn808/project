c11d2c13efef20a071f53313426e86abec0537fd
Malikov-Afruz <afruzmalikov65@gmail.com>
2024-07-13 20:39:56 +0500

    #Update models,add oauth

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -17,4 +17,15 @@ const userSchema = new mongoose.Schema(
 	{ timestamps: true }
 )
 
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) return next()
+	const salt = await bcrypt.genSalt(10)
+	this.password = await bcrypt.hash(this.password, salt)
+	next()
+})
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+	return bcrypt.compare(candidatePassword, this.password)
+}
+
 module.exports = mongoose.model('users', userSchema)
