c3435fb2fb037a4569c69603a2ae1c0bbd67994a
Malikov-Afruz <afruzmalikov65@gmail.com>
2024-07-13 19:32:28 +0500

    #Add,Update

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -2,25 +2,19 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema(
 	{
+		socialId: { type: String, required: true, unique: true },
+		familyName: { type: String },
+		givenName: { type: String },
 		username: {
 			type: String,
-			required: true,
 			unique: true,
 		},
-		password: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true },
+		password: { type: String, unique: true },
+		userPhotoLink: { type: String },
 	},
 
 	{ timestamps: true }
 )
-userSchema.pre('save', async function (next) {
-	if (!this.isModified('password')) return next()
-	const salt = await bcrypt.genSalt(10)
-	this.password = await bcrypt.hash(this.password, salt)
-	next()
-})
 
-userSchema.methods.comparePassword = function (candidatePassword) {
-	return bcrypt.compare(candidatePassword, this.password)
-}
-
-module.exports = mongoose.model('user', userSchema)
+module.exports = mongoose.model('users', userSchema)
