d04e5b41aa4d942c298795a9f3d3bc20605f9df4
aaa <aaa>
2024-07-18 16:02:22 +0500

    add product api

--- backend/models/ProductSchema.js
+++ backend/models/ProductSchema.js
@@ -18,3 +18,43 @@ const userSchema = new mongoose.Schema(
 )
 
 module.exports = mongoose.model('product', userSchema)
+
+
+
+// const mongoose = require('mongoose');
+// const { body } = require('express-validator')
+
+
+// const ProductSchema = new mongoose.Schema({
+// 	imageUrl: {
+// 		type: String,
+// 		required: true,
+// 		unique: true,
+// 		minlength: 5,
+// 		maxlength: 20
+// 	},
+// 	title: {
+// 		type: String,
+// 		required: true,
+// 		unique: true,
+// 		minlength: 4,
+// 		maxlength: 10
+// 	},
+// 	description: {
+// 		type: String,
+// 		required: true,
+// 		unique: true,
+// 		minlength: 10,
+// 		maxlength: 20
+// 	}
+// }, {
+// 	timestamps: true
+// });
+
+// export const ProductValidation = [
+// 	body('imageUrl', 'Заполнение обязательно').isURL(),
+// 	body('title', 'Должно быть название, не менее 4 символов').isLength({ min: 4 }),
+// 	body('description', 'Информация об этом товаре'),
+// ];
+
+// export default mongoose.model('Product', ProductSchema);
