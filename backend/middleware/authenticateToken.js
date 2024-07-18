210fea0ef93dd25f00f0e8640f35ea8d4c25b680
aaa <aaa>
2024-07-18 14:08:55 +0500

    merge oauth from Afruz

---
+++ backend/middleware/authenticateToken.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+function authenticateToken(req, res, next) {
+	const token = req.cookies.accessToken
+	if (!token) {
+		console.log('Access token not found')
+		return next() 
+	}
+
+	jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+		if (err) {
+			console.log('Error verifying token:', err)
+			return res.sendStatus(403)
+		}
+		req.user = user
+		console.log('Authenticated user:', user)
+		next()
+	})
+}
+
+module.exports = authenticateToken
---
+++ backend/routes/products.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const productSchema = require('../models/ProductSchema')
+require('dotenv').config()
+const router = express.Router()
+module.exports = router
