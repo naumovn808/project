f145970ed5eede10e97fedfa8c8c2356d2e380d2
Malikov-Afruz <afruzmalikov65@gmail.com>
2024-07-20 16:30:24 +0500

    #UPDATE auth_test

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -3,8 +3,7 @@ const jwt = require('jsonwebtoken')
 function authenticateToken(req, res, next) {
 	const token = req.cookies.accessToken
 	if (!token) {
-		console.log('Access token not found')
-		return next() // Продолжить выполнение следующего middleware или маршрута
+		return res.status(401).send({ message: 'Unauthorized User!!!' })
 	}
 
 	jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
