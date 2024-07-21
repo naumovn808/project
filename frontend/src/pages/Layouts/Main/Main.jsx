9bf19e3c91635d936142df08f3234776e49bee2d
onlive1337 <tabdulov2006@gmail.com>
2024-07-22 01:56:09 +0500

    Create SocialButton and add Social Icons (example of social button in main)

--- frontend/src/pages/Layouts/Main/Main.jsx
+++ frontend/src/pages/Layouts/Main/Main.jsx
@@ -3,12 +3,17 @@ import { Outlet } from "react-router-dom";
 import styles from './Main.module.css'
 import Auth_Footer from "../../../components/Auth_Footer/Auth_Footer";
 import Auth_Header from '../../../components/Auth_Header/Auth_Header'
+import SocialButton from "../../../components/SocialButton/SocialButton";
 
 const Main = () => {
     return (
         <div className={styles.main_page}>
             <Auth_Header/>
             <Auth_Footer/>
+            <SocialButton
+            iconSrc="/VK.png"
+            text="ВКонтакте"
+            />
         </div>
 
 
