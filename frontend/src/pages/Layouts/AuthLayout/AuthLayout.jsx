6bbeee3e74e54ae07882c10241bd1e7a4d4db6be
MaximK38 <hpmax38@gmail.com>
2024-07-22 13:52:33 +0500

    loginAuth

--- frontend/src/pages/Layouts/AuthLayout/AuthLayout.jsx
+++ frontend/src/pages/Layouts/AuthLayout/AuthLayout.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import styles from "./AuthLayout.module.css";
 import AuthHeader from "../../../components/Auth_Header/Auth_Header.jsx";
 import AuthFooter from "../../../components/Auth_Footer/Auth_Footer.jsx";
+import LoginClick from "../../Auth/LoginClick/LoginClick.jsx";
 
 const AuthLayout = () => {
+	const location = useLocation();
+
 	return (
 		<div className={styles.container__outlet}>
 			<div>
 				<AuthHeader isAuthPage={true} />
+				{location.pathname === "/auth/login" && <div className={styles.login__card}><LoginClick/></div>}
 				<div className={styles.outlet__card}>
 					<Outlet />
 				</div>
