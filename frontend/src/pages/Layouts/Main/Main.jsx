11db5c46b1901f5f24194e78c53c4ea9b6d8f7db
MaximK38 <hpmax38@gmail.com>
2024-07-29 03:16:25 +0500

    main_mobile

--- frontend/src/pages/Layouts/Main/Main.jsx
+++ frontend/src/pages/Layouts/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import styles from "./Main.module.css";
 import Auth_Footer from "../../../components/Auth_Footer/Auth_Footer";
@@ -7,6 +7,24 @@ import CocktailsFilters from "../../../components/CocktailsFilters/CocktailsFilt
 import Button from "../../../components/Button/Button";
 
 const Main = () => {
+	const [isButtonsVisible, setIsButtonsVisible] = useState(true);
+	const footerRef = useRef(null);
+
+	// mobile buttons
+	useEffect(() => {
+		const handleScroll = () => {
+			if (footerRef.current) {
+				const footerTop = footerRef.current.getBoundingClientRect().top;
+				const windowHeight = window.innerHeight;
+				setIsButtonsVisible(footerTop > windowHeight);
+			}
+		};
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
 		<div className={styles.main_page}>
 			<Auth_Header />
@@ -14,27 +32,35 @@ const Main = () => {
 				<div className={styles.filters}>
 					<CocktailsFilters />
 				</div>
+
 				<div className={styles.content}>
 					<div className={styles.content__cards}>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
-						<div className={styles.card}></div>
+						{[...Array(10)].map((o, i) => (
+							<div key={i} className={styles.card}></div>
+						))}
 						<Outlet />
 					</div>
+
 					<div className={styles.content__button}>
 						<Button title={"Показать еще коктейли"} className={styles.content__button__add} />
 					</div>
+
+					{/* mobile */}
+					<div className={`${styles.content__mobile} ${!isButtonsVisible ? styles.hidden : ""}`}>
+						<div className={styles.content__mobile__buttons}>
+							<button className={styles.button}>
+								<img className={styles.button__img} src="./Filter.png" alt="filter" />
+							</button>
+							<button className={styles.button} onClick={() => window.scroll({ top: 0 })}>
+								<img className={styles.button__arrow} src="./ArrowTop.png" alt="Arrow" />
+							</button>
+						</div>
+					</div>
 				</div>
 			</div>
-			<Auth_Footer />
+			<div ref={footerRef}>
+				<Auth_Footer />
+			</div>
 		</div>
 	);
 };
